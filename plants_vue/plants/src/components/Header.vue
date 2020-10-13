ab7b22226b5a2f35f3663fde4c9db86f884e0f0c
hyylc <1959232004@qq.com>
2020-10-13 20:06:24 +0800

    创建vue项目，尝试一点页面

---
+++ plants_vue/plants/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <b-container>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">植物信息检索</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Link0</b-nav-item>
+                <b-nav-item href="#">Link1</b-nav-item>
+                <b-nav-item href="#">Link2</b-nav-item>
+                <b-nav-item href="#">Link3</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-nav-form>
+
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                    <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </b-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang='scss' scoped>//lang告诉解释其css符合什么编译器的语法；scoped：当前vue文件生效，没有scoped则全局生效
+
+</style>
---
+++ plants_vue/plants/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <Header />
+    <b-container>
+      <div style="height:1000px;background-color:red">
+        body部分
+      </div>
+    </b-container>
+
+    <b-container>
+      <div style="height:50px;background-color:blue">
+        footer部分
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue"
+export default {
+  name: "home",
+  components: {
+    Header
+  }
+};
+</script>
